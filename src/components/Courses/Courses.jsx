c71e7ef3c7d776e3325d6ea69d59deb9a2e26397
Ripon Mondal <riponmondal149@gmail.com>
2024-02-21 01:13:27 +0600

    complete the design ui part of corses section

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import './Courses.css';
+import Course from '../Course/Course';
 
 const Courses = () => {
+    const [courses, setCourses] = useState([]);
+
+    fetch('courses.json')
+        .then(res => res.json())
+        .then(data => setCourses(data))
+
     return (
         <div className='courses-container'>
-            <div>
-                <h3>All Courses here</h3>
+            <div className='single-courses'>
+                {
+                    courses.map(course => <Course
+                        key={course.id}
+                        course={course}
+                    ></Course>)
+                }
+
             </div>
             <div>
                 <h3>Course info after click hare</h3>
