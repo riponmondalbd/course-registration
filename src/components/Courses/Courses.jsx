852e43098f6205465feecbc7d26a3ee776150be3
Ripon Mondal <riponmondal149@gmail.com>
2024-02-20 23:57:13 +0600

    colplete heading section named couse registration and also make course options 2 part one for all courses here and another for course info

---
+++ src/components/Courses/Courses.jsx
@@ -0,0 +1,17 @@
+import './Courses.css';
+
+const Courses = () => {
+    return (
+        <div className='courses-container'>
+            <div>
+                <h3>All Courses here</h3>
+            </div>
+            <div>
+                <h3>Course info after click hare</h3>
+            </div>
+        </div>
+
+    );
+};
+
+export default Courses;
