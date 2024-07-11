915b7675ece95333dfcf49799a14d4381a3bf316
riponmondalbd <riponmondal149@gmail.com>
2024-07-11 07:26:28 +0600

    course calculation part ui design complete and also complete the ui

--- src/components/CourseCalculations/CourseCalculations.jsx
+++ src/components/CourseCalculations/CourseCalculations.jsx
@@ -4,7 +4,15 @@ import PropTypes from "prop-types";
 const CourseCalculations = () => {
   return (
     <div className="h-1/2 md:w-1/4 bg-white p-6 rounded-xl">
-      <h4>Credit Hour Remaining 7 hr</h4>
+      <h4 className="text-lg font-bold text-[#2F80ED]">
+        Credit Hour Remaining 7 hr
+      </h4>
+      <hr className="mt-4 mb-4 border-[1px]" />
+      <h2 className="text-[20px] font-bold">Course Name</h2>
+      <hr className="mt-4 mb-4 border-[1px]" />
+      <p className="text-base font-medium">Total Credit Hour :</p>
+      <hr className="mt-4 mb-4 border-[1px]" />
+      <p className="text-base font-semibold">Total Credit Hour : USD</p>
     </div>
   );
 };
